96f8bbbc69e9e017dd13e2d07ac3b434b84b98cd
imwrme <comfortable.in.tech@gmail.com>
2018-02-19 20:01:33 +0900

    삼각형 구하긔

---
+++ printTriangle.js
@@ -0,0 +1,14 @@
+function printTriangle(num) {
+    var result = '';
+    for(var j=1; j<=num; j++){
+      for(var i=0; i<j; i++){
+          result +="*";
+      }
+      result +="\n";
+    }
+  
+  
+    // 함수를 완성하세요
+    return result;
+  }
+  
